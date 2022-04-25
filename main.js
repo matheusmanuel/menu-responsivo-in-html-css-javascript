96e6d066896c4a00e073509ce36bd47dde69f4a1
Matheus Manuel <80291248+matheusmanuel@users.noreply.github.com>
2022-04-25 22:35:57 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var ul = document.querySelector('nav ul');
+var menuBtn = document.querySelector('.menu-btn i');
+
+function menuShow() {
+    if (ul.classList.contains('open')) {
+        ul.classList.remove('open');
+    }else{
+        ul.classList.add('open');
+    }
+}
